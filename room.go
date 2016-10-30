30511fb60d3a1c2955dec3400869a302a7a05ea8
ken5scal <kengoscal@gmail.com>
2016-10-31 00:52:02 +0900

    Add room.go

--- room.go
+++ room.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
 type room struct {
 	//channel that olds message pending to transfer
 	fwd_chan chan []byte
@@ -11,6 +18,35 @@ type room struct {
 	leave_chan chan *client
 
 	clients map[*client]bool
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  socketBufferSize,
+	WriteBufferSize: socketBufferSize,
+}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+	client := &client{
+		socket:    socket,
+		send_chan: make(chan []byte, messageBufferSize),
+		room:      r,
+	}
+
+	r.join_chan <- client
+	defer func() { r.leave_chan <- client }()
+
+	go client.write()
+	client.read()
 }
 
 func (r *room) run() {
