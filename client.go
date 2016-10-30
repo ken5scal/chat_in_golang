aa829193afb5e0d957d52f9a0da048d52708384a
ken5scal <kengoscal@gmail.com>
2016-10-30 23:31:11 +0900

    Make a comment

--- client.go
+++ client.go
@@ -3,7 +3,9 @@ package main
 import "golang.org/x/net/websocket"
 
 type client struct {
+	// Socket for client
 	socket *websocket.Conn
-	send   chan []byte
-	room   *room
+	// Channel where message is send
+	send_chan chan []byte
+	room      *room
 }
