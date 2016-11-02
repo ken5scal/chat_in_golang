c5735af3e6fde4f698bb09c6babc6f8ef49882a2
ken5scal <kengoscal@gmail.com>
2016-11-02 09:19:29 +0900

    Update test

--- trace/tracer_test.go
+++ trace/tracer_test.go
@@ -1,19 +1,18 @@
 package trace
 
 import (
-	"testing"
 	"bytes"
-
+	"testing"
 )
 
 func TestNew(t *testing.T) {
 	var buf bytes.Buffer
 	tracer := New(&buf)
-	if tracer ==nil {
+	if tracer == nil {
 		t.Error("Tracer is null")
 	} else {
 		tracer.Trace("Hello trace package")
-		if buf.String() != "Hello trace package" {
+		if buf.String() != "Hello trace package\n" {
 			t.Errorf("'%s' is not expected", buf.String())
 		}
 	}
