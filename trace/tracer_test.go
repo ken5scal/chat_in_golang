ba8b19c9fbea4e1589abedb05ed056ebc91f2c06
ken5scal <kengoscal@gmail.com>
2016-11-02 09:04:50 +0900

    Modify test method

--- trace/tracer_test.go
+++ trace/tracer_test.go
@@ -2,8 +2,19 @@ package trace
 
 import (
 	"testing"
+	"bytes"
+
 )
 
 func TestNew(t *testing.T) {
-	t.Error("NO test")
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer ==nil {
+		t.Error("Tracer is null")
+	} else {
+		tracer.Trace("Hello trace package")
+		if buf.String() != "Hello trace package" {
+			t.Errorf("'%s' is not expected", buf.String())
+		}
+	}
 }
