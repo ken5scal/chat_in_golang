b9b51588c26c61063adeb42208bfc3a043266050
ken5scal <kengoscal@gmail.com>
2016-11-03 15:36:29 +0900

    Parse response from OAuth provider

--- chat/auth.go
+++ chat/auth.go
@@ -3,8 +3,10 @@ package main
 import (
 	"net/http"
 	"strings"
-	"log"
 	"fmt"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/objx"
+	"log"
 )
 
 type authHandler struct {
@@ -39,7 +41,49 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	provider := segs[3]
 	switch action {
 	case "login":
-		log.Println("TODO: ", provider)
+		// Retrieving OAuth Provider Object from URL
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error when trying to get provider %s: %s", provider, err), http.StatusBadRequest)
+			return
+		}
+
+		// Retriving URL for staring OAuth process
+		loginUrl, err := provider.GetBeginAuthURL(nil, nil)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error when trying to GetBeginAuthURL for %s: %s", provider, err), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Location", loginUrl)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	case "callback":
+		// Retrieving OAuth Provider Object from URL
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error when trying to get provider %s: %s", provider, err), http.StatusBadRequest)
+			return
+		}
+
+		creds, err := provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
+		if err != nil {
+			log.Fatalln("Authentication did not finiesh", provider, "-", err)
+		}
+
+		user, err := provider.GetUser(creds)
+		if err != nil {
+			log.Fatalln("Failed fetchin user info", provider, "-", err)
+		}
+
+		authCookieVlaue :=
+			objx.New(map[string]interface{}{"name": user.Name(), }).
+				MustBase64()
+		http.SetCookie(w, &http.Cookie{
+			Name: "auth",
+			Value: authCookieVlaue,
+			Path: "/"})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
 	default:
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "Action %s not supported", action)
